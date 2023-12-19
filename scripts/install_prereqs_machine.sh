b398003830653797c3d68436e61ac74eae9e81ab
Vijay Pandian <ananth.vijay@gmail.com>
2023-12-19 10:40:09 -0500

    Build from binary - terraform

--- scripts/install_prereqs_machine.sh
+++ scripts/install_prereqs_machine.sh
@@ -32,22 +32,7 @@ eksctl version
 echo "----------------------------------------"
 echo "        Installing Terraform"
 echo "----------------------------------------"   
-sudo apt-get update && sudo apt-get install -y gnupg software-properties-common
-
-sudo wget -O- https://apt.releases.hashicorp.com/gpg | \
-gpg --dearmor | \
-sudo tee /usr/share/keyrings/hashicorp-archive-keyring.gpg
-
-
-sudo gpg --no-default-keyring \
---keyring /usr/share/keyrings/hashicorp-archive-keyring.gpg \
---fingerprint
-
-echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] \
-https://apt.releases.hashicorp.com $(lsb_release -cs) main" | \
-sudo tee /etc/apt/sources.list.d/hashicorp.list
-
-
-sudo apt update -y
-
-sudo apt-get install terraform=1.6.0 -y
+git clone --depth 1 --branch 1.6.0 https://github.com/hashicorp/terraform
+cd terraform
+go install
+echo $PATH
