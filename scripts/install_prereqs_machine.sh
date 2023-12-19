b0350a9eda327228372b3fc329b405d688f16b72
Vijay Pandian <ananth.vijay@gmail.com>
2023-12-19 10:33:22 -0500

    Update install_prereqs_machine.sh - tf steps

--- scripts/install_prereqs_machine.sh
+++ scripts/install_prereqs_machine.sh
@@ -32,11 +32,22 @@ eksctl version
 echo "----------------------------------------"
 echo "        Installing Terraform"
 echo "----------------------------------------"   
-curl -fsSL https://apt.releases.hashicorp.com/gpg | sudo apt-key add -       
-sudo apt-add-repository "deb [arch=amd64] https://apt.releases.hashicorp.com $(lsb_release -cs) main"        
-sudo apt-get -y update  
+sudo apt-get update && sudo apt-get install -y gnupg software-properties-common
 
-echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] 
-https://apt.releases.hashicorp.com $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/hashicorp.list
+sudo wget -O- https://apt.releases.hashicorp.com/gpg | \
+gpg --dearmor | \
+sudo tee /usr/share/keyrings/hashicorp-archive-keyring.gpg
+
+
+sudo gpg --no-default-keyring \
+--keyring /usr/share/keyrings/hashicorp-archive-keyring.gpg \
+--fingerprint
+
+echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] \
+https://apt.releases.hashicorp.com $(lsb_release -cs) main" | \
+sudo tee /etc/apt/sources.list.d/hashicorp.list
+
+
+sudo apt update -y
 
 sudo apt-get install terraform=1.6.0 -y
