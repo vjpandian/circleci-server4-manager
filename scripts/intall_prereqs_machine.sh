29cf52f746ad72d173c9d7b48d601b92908a5fe4
Vijay Pandian <ananth.vijay@gmail.com>
2023-12-19 10:15:14 -0500

    Create intall_prereqs_machine.sh

---
+++ scripts/intall_prereqs_machine.sh
@@ -0,0 +1,25 @@
+echo "Installing Helm..."
+curl -LO https://get.helm.sh/helm-v3.11.2-linux-amd64.tar.gz
+tar -zxvf helm-v3.11.2-linux-amd64.tar.gz
+sudo mv linux-amd64/helm /usr/local/bin/helm
+helm version
+
+echo "Installing Helm Diff..."
+helm plugin install https://github.com/databus23/helm-diff
+
+
+echo "Installing kubectl..."
+curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+curl -LO "https://dl.k8s.io/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl.sha256"
+
+echo "$(cat kubectl.sha256)  kubectl" | sha256sum --check
+sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
+kubectl version --client
+
+echo "Installing eksctl..."
+curl --silent --location "https://github.com/weaveworks/eksctl/releases/latest/download/eksctl_$(uname -s)_amd64.tar.gz" | tar xz -C /tmp
+sudo mv /tmp/eksctl /usr/local/bin
+eksctl version
+            
+sudo apt policy terraform
+sudo apt install terraform=$TF_VERSION
