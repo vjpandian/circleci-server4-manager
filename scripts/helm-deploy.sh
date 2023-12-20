46d05012d49ee705bf278beb9a4bb6013beb1280
Vijay Pandian <ananth.vijay@gmail.com>
2023-12-20 18:34:00 -0500

    Update helm-deploy.sh - update env_vars

--- scripts/helm-deploy.sh
+++ scripts/helm-deploy.sh
@@ -1,2 +1,2 @@
 helm registry login cciserver.azurecr.io -u $AZURECR_USER -p $AZURECR_PWD
-helm upgrade $RELEASE_NAME oci://cciserver.azurecr.io/circleci-server -n $NAMESPACE --version $SERVER_VERSION -f $VALUES_FILE --username $CUSTENG_AZURECR_USER --password $CUSTENG_AZURECR_PWD
+helm upgrade $RELEASE_NAME oci://cciserver.azurecr.io/circleci-server -n $NAMESPACE --version $SERVER_VERSION -f $VALUES_FILE --username $AZURECR_USER --password $AZURECR_PWD
