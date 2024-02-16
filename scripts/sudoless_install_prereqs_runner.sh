e8d6e9d5e41a75b25e80cba436c3a285424b595b
Vijay Pandian <ananth.vijay@gmail.com>
2024-02-16 12:08:52 -0500

    Create sudoless_install_prereqs_runner.sh

---
+++ scripts/sudoless_install_prereqs_runner.sh
@@ -0,0 +1,40 @@
+echo "----------------------------------------"
+echo "        Installing Helm"
+echo "----------------------------------------"
+curl -LO https://get.helm.sh/helm-v3.11.2-linux-amd64.tar.gz
+tar -zxvf helm-v3.11.2-linux-amd64.tar.gz
+mv linux-amd64/helm /usr/local/bin/helm
+helm version
+
+echo "----------------------------------------"
+echo "        Installing Helm-Diff"
+echo "----------------------------------------"
+helm plugin install https://github.com/databus23/helm-diff
+
+
+echo "----------------------------------------"
+echo "        Installing kubectl"
+echo "----------------------------------------"
+curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+curl -LO "https://dl.k8s.io/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl.sha256"
+
+echo "$(cat kubectl.sha256)  kubectl" | sha256sum --check
+install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
+kubectl version --client
+
+echo "----------------------------------------"
+echo "        Installing eksctl"
+echo "----------------------------------------"
+curl --silent --location "https://github.com/weaveworks/eksctl/releases/latest/download/eksctl_$(uname -s)_amd64.tar.gz" | tar xz -C /tmp
+mv /tmp/eksctl /usr/local/bin
+eksctl version
+
+echo "----------------------------------------"
+echo "        Installing Terraform"
+echo "----------------------------------------"   
+-fLSs
+rm -rf terraform/
+apt install unzip zip -y
+curl -fLSs "https://releases.hashicorp.com/terraform/1.6.0/terraform_1.6.0_linux_amd64.zip" -o "terraform_1.6.0.zip"
+unzip -o terraform_1.6.0.zip
+mv terraform /usr/local/bin/
