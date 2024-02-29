91f579a20a3afcca6a8c03bd952e8386d7a235d7
Vijay Pandian <ananth.vijay@gmail.com>
2024-02-29 15:41:45 -0700

    Create create-namespace.sh

---
+++ scripts/create-namespace.sh
@@ -0,0 +1,20 @@
+
+eksctl utils write-kubeconfig --cluster=$CLUSTER
+
+# Check if the namespace already exists
+regcred_exists=$(kubectl get secret $REGCRED_SECRET -n $NAMESPACE 2>/dev/null)
+
+
+if [ -z "$regcred_exists" ]; then
+  # Create the namespace
+  # Create regcred secret
+  kubectl -n $NAMESPACE create secret docker-registry $REGCRED_SECRET \
+    --docker-server=https://cciserver.azurecr.io \
+    --docker-username=$CUSTENG_AZURECR_USER \
+    --docker-password=$CUSTENG_AZURECR_PWD \
+    --docker-email=vijay@circleci.com \
+    --dry-run=client -o yaml | kubectl -n $NAMESPACE apply -f -
+  echo "Secret $REGCRED_SECRET was created in $NAMESPACE"
+else
+  echo "$REGCRED_SECRET already exists....will skip creation"
+fi
