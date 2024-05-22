6d0c4f5f63c0df071c6fda85d952339bf498c883
Vijay Pandian <ananth.vijay@gmail.com>
2024-05-22 16:58:11 -0400

    Update install_prerequisites.sh - provide eksctl version

--- scripts/install_prerequisites.sh
+++ scripts/install_prerequisites.sh
@@ -23,3 +23,4 @@ curl -sLO "https://github.com/eksctl-io/eksctl/releases/latest/download/eksctl_$
 curl -sL "https://github.com/eksctl-io/eksctl/releases/latest/download/eksctl_checksums.txt" | grep $PLATFORM | sha256sum --check
 tar -xzf eksctl_$PLATFORM.tar.gz -C /tmp && rm eksctl_$PLATFORM.tar.gz
 sudo mv /tmp/eksctl /usr/local/bin
+eksctl version
