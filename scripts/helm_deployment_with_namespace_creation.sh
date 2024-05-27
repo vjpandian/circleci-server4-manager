b4b5df4907550b22e1090977c566432daedf7a83
Vijay Pandian <ananth.vijay@gmail.com>
2024-05-27 11:56:05 -0400

    move values file conditions to script

--- scripts/helm_deployment_with_namespace_creation.sh
+++ scripts/helm_deployment_with_namespace_creation.sh
@@ -5,8 +5,23 @@ if [ -z "${NAMESPACE}" ]; then
   exit 1
 fi
 
+major_version=$(echo "$HELM_VERSION" | cut -d '.' -f 1)
+minor_version=$(echo "$HELM_VERSION" | cut -d '.' -f 2)
 
-if [ -z "${values_file}" ]; then
+# Determine which YAML file to use based on the version
+if [[ "$major_version" -eq 4 && "$minor_version" -le 2 ]]; then
+    VALUES_FILE="$NAMESPACE-pre_43_helm-values.yaml"
+    echo "$VALUES_FILE will be used for helm deploy"
+elif [[ "$major_version" -eq 4 && "$minor_version" -ge 3 && "$minor_version" -le 5 ]]; then
+    VALUES_FILE="$NAMESPACE-post_43_helm-values.yaml"
+    echo "$VALUES_FILE will be used for helm deploy"
+else
+    echo "Version not supported: $HELM_VERSION"
+    exit 1
+fi
+
+
+if [ -z "${VALUES_FILE}" ]; then
   echo "Helm values var is not set. Exiting..."
   exit 1
 fi
@@ -16,12 +31,12 @@ namespace_exists=$(kubectl get namespace $NAMESPACE 2>/dev/null --ignore-not-fou
 if [ -z "$namespace_exists" ]; then
     echo "Namespace $NAMESPACE will be created."
     kubectl create ns $NAMESPACE
-    echo "helm install $values_file $HELM_VERSION will be run"
+    echo "helm install $VALUES_FILE $HELM_VERSION will be run"
     helm registry login cciserver.azurecr.io -u $CUSTENG_AZURECR_USER -p $CUSTENG_AZURECR_PWD
-    helm install cci-$NAMESPACE oci://cciserver.azurecr.io/circleci-server -n $NAMESPACE --version $HELM_VERSION -f ${values_file} --username $CUSTENG_AZURECR_USER --password $CUSTENG_AZURECR_PWD
+    helm install cci-$NAMESPACE oci://cciserver.azurecr.io/circleci-server -n $NAMESPACE --version $HELM_VERSION -f ${VALUES_FILE} --username $CUSTENG_AZURECR_USER --password $CUSTENG_AZURECR_PWD
 else
     echo "Namespace $NAMESPACE already exists...will skip namespace creation"
-    echo "helm upgrade --install $values_file $HELM_VERSION will be run"
+    echo "helm upgrade --install $VALUES_FILE $HELM_VERSION will be run"
     helm registry login cciserver.azurecr.io -u $CUSTENG_AZURECR_USER -p $CUSTENG_AZURECR_PWD
-    helm upgrade --install cci-$NAMESPACE oci://cciserver.azurecr.io/circleci-server -n $NAMESPACE --version $HELM_VERSION -f ${values_file} --username $CUSTENG_AZURECR_USER --password $CUSTENG_AZURECR_PWD
+    helm upgrade --install cci-$NAMESPACE oci://cciserver.azurecr.io/circleci-server -n $NAMESPACE --version $HELM_VERSION -f ${VALUES_FILE} --username $CUSTENG_AZURECR_USER --password $CUSTENG_AZURECR_PWD
 fi
