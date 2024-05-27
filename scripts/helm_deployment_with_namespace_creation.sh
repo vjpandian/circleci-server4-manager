d4257fc38cbb105daeeb061b76b3f2327910c98f
Vijay Pandian <ananth.vijay@gmail.com>
2024-05-27 10:57:29 -0400

    Create helm_deployment_with_namespace_creation.sh

---
+++ scripts/helm_deployment_with_namespace_creation.sh
@@ -0,0 +1,16 @@
+
+
+namespace_exists=$(kubectl get namespace $NAMESPACE 2>/dev/null --ignore-not-found)
+
+if [ -z "$namespace_exists" ]; then
+    echo "Namespace $NAMESPACE will be created."
+    kubectl create ns $NAMESPACE
+    echo "helm install $values_file $HELM_VERSION will be run"
+    helm registry login cciserver.azurecr.io -u $CUSTENG_AZURECR_USER -p $CUSTENG_AZURECR_PWD
+    helm install cci-$NAMESPACE oci://cciserver.azurecr.io/circleci-server -n $NAMESPACE --version $HELM_VERSION -f ${values_file} --username $CUSTENG_AZURECR_USER --password $CUSTENG_AZURECR_PWD
+else
+    echo "Namespace $NAMESPACE already exists...will skip namespace creation"
+    echo "helm upgrade --install $values_file $HELM_VERSION will be run"
+    helm registry login cciserver.azurecr.io -u $CUSTENG_AZURECR_USER -p $CUSTENG_AZURECR_PWD
+    helm upgrade --install cci-$NAMESPACE oci://cciserver.azurecr.io/circleci-server -n $NAMESPACE --version $HELM_VERSION -f ${values_file} --username $CUSTENG_AZURECR_USER --password $CUSTENG_AZURECR_PWD
+fi
