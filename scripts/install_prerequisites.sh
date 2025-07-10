f914f229caa232ef8b416c8165c4abefe341a170
Vijay Pandian <ananth.vijay@gmail.com>
2025-07-10 13:33:06 -0400

    Add bash shebang

--- scripts/install_prerequisites.sh
+++ scripts/install_prerequisites.sh
@@ -1,3 +1,6 @@
+#!/bin/bash
+set -euo pipefail
+
 echo "Installing Helm..."
 curl -LO https://get.helm.sh/helm-v3.11.2-linux-amd64.tar.gz
 tar -zxvf helm-v3.11.2-linux-amd64.tar.gz
@@ -17,10 +20,11 @@ sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
 kubectl version --client
 
 ARCH=amd64
-PLATFORM=$(uname -s)_$ARCH
-curl -sLO "https://github.com/eksctl-io/eksctl/releases/latest/download/eksctl_$PLATFORM.tar.gz"
-# (Optional) Verify checksum
-curl -sL "https://github.com/eksctl-io/eksctl/releases/latest/download/eksctl_checksums.txt" | grep $PLATFORM | sha256sum --check
-tar -xzf eksctl_$PLATFORM.tar.gz -C /tmp && rm eksctl_$PLATFORM.tar.gz
+PLATFORM=$(uname -s | tr '[:upper:]' '[:lower:]')_$ARCH
+
+echo "Installing eksctl..."
+curl -sLO "https://github.com/eksctl-io/eksctl/releases/latest/download/eksctl_${PLATFORM}.tar.gz"
+curl -sL "https://github.com/eksctl-io/eksctl/releases/latest/download/eksctl_checksums.txt" | grep "${PLATFORM}.tar.gz" | sha256sum --check
+tar -xzf eksctl_${PLATFORM}.tar.gz -C /tmp && rm eksctl_${PLATFORM}.tar.gz
 sudo mv /tmp/eksctl /usr/local/bin
 eksctl version
