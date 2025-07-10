3831a84e26313e46d35ac3c658b5051df7eb88fe
Vijay Pandian <ananth.vijay@gmail.com>
2025-07-10 13:38:34 -0400

    Update install_prerequisites.sh

--- scripts/install_prerequisites.sh
+++ scripts/install_prerequisites.sh
@@ -22,9 +22,11 @@ kubectl version --client
 ARCH=amd64
 PLATFORM=$(uname -s | tr '[:upper:]' '[:lower:]')_$ARCH
 
-echo "Installing eksctl..."
-curl -sLO "https://github.com/eksctl-io/eksctl/releases/latest/download/eksctl_${PLATFORM}.tar.gz"
-curl -sL "https://github.com/eksctl-io/eksctl/releases/latest/download/eksctl_checksums.txt" | grep "${PLATFORM}.tar.gz" | sha256sum --check
-tar -xzf eksctl_${PLATFORM}.tar.gz -C /tmp && rm eksctl_${PLATFORM}.tar.gz
-sudo mv /tmp/eksctl /usr/local/bin
-eksctl version
+# for ARM systems, set ARCH to: `arm64`, `armv6` or `armv7`
+ARCH=amd64
+PLATFORM=$(uname -s)_$ARCH
+curl -sLO "https://github.com/eksctl-io/eksctl/releases/latest/download/eksctl_$PLATFORM.tar.gz"
+# (Optional) Verify checksum
+curl -sL "https://github.com/eksctl-io/eksctl/releases/latest/download/eksctl_checksums.txt" | grep $PLATFORM | sha256sum --check
+tar -xzf eksctl_$PLATFORM.tar.gz -C /tmp && rm eksctl_$PLATFORM.tar.gz
+sudo install -m 0755 /tmp/eksctl /usr/local/bin && rm /tmp/eksctl
