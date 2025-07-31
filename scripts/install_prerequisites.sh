13dd7767901f8eea864802abf6632cc8e15efa43
Vijay Pandian <ananth.vijay@gmail.com>
2025-07-31 12:11:45 -0400

    Update install_prerequisites.sh

--- scripts/install_prerequisites.sh
+++ scripts/install_prerequisites.sh
@@ -2,8 +2,8 @@
 set -euo pipefail
 
 echo "Installing Helm..."
-curl -LO https://get.helm.sh/helm-v3.11.2-linux-amd64.tar.gz
-tar -zxvf helm-v3.11.2-linux-amd64.tar.gz
+curl -LO https://get.helm.sh/helm-v3.15.3-linux-amd64.tar.gz
+tar -zxvf helm-v3.15.3-linux-amd64.tar.gz
 sudo mv linux-amd64/helm /usr/local/bin/helm
 helm version
 
